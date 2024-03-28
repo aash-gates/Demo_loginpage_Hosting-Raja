64d18f7a2430df31053e27b6d24d35d8d9cb32e1
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:19:40 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,7 @@
 // Database configuration
 $db_host = "localhost"; // Hostname of the database server
 $db_username = "root"; // Username for database access
+$db_password = ""; // Password for database access
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
