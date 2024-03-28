15ab9e6c5d4bdaf8cc05a892c9df05418ff2f8bd
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:20:00 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,6 +8,7 @@ $db_name = "SMS.sql"; // Name of the database
 // Attempt to establish a connection to the database
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
+// Check if the connection was successful
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
