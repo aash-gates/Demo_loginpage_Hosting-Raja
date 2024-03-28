17a68a4a6da025f605849e8d67c8b24e5994e452
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:48:47 +0530

    Update student_details.php

--- student_details.php
+++ student_details.php
@@ -1,13 +1,3 @@
-<?php
-// Include your db_connect.php file to establish a database connection
-include 'db_connect.php';
-
-// Check if connection is established
-if (!$connection) {
-    // If connection failed, display error message and terminate script
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 // Retrieve student details based on the ID passed through GET parameter
 if (isset($_GET['id'])) {
     $student_id = $_GET['id'];
