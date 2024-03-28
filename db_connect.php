bb78353dcfbeba9d061a5cae6e8fba5c0ceda53a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:14:30 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,3 @@
-// Check if the connection was successful
 if (!$connection) {
     // If connection failed, display error message and terminate script
     die("Connection failed: " . mysqli_connect_error());
