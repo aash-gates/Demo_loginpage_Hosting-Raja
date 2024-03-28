dc6ec0566c94a29e1700ad22358584126086016a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:19:48 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,6 +5,7 @@ $db_username = "root"; // Username for database access
 $db_password = ""; // Password for database access
 $db_name = "SMS.sql"; // Name of the database
 
+// Attempt to establish a connection to the database
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
