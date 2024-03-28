768552356cb116bfdf6a14bacf8db5365f3fd513
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:15:27 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,6 +5,7 @@ $dbusername = "root";
 $dbpassword = "";
 // Create connection
 $conn = mysqli_connect($dbservername, $dbusername, $dbpassword);
+// Check connection
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
