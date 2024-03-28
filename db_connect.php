886b88b42d6ccac9082eb45a05decf1edd5a4009
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-28 22:19:46 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,6 +4,7 @@ $db_host = "localhost"; // Hostname of the database server
 $db_username = "root"; // Username for database access
 $db_password = ""; // Password for database access
 $db_name = "SMS.sql"; // Name of the database
+
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
